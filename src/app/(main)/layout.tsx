507843321b2c88a8358a646cfd53663a4711c1e3
abhay6969 <abhaytripathi6969@gmail.com>
2025-02-12 19:17:49 +0530

    sidebar

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,14 @@
+type Props = {
+	children: React.ReactNode;
+};
+
+const MainLayout = ({ children }: Props) => {
+	return (
+		<>
+			<main className="pl-[256px]  h-full">
+				<div className="bg-red-500 h-full">{children}</div>
+			</main>
+		</>        
+	);
+};
+export default MainLayout; 
